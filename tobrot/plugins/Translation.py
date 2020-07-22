adb463b23ce654721df6dfa3a33b125e720b452b
madibag <62722024+madibag@users.noreply.github.com>
2020-07-22 22:22:48 +0300

    Delete Translation.py

--- tobrot/plugins/Translation.py
+++
@@ -1,7 +0,0 @@
-class Translation(object):
-    HELP = """This bot can do a lot of things 
-    To download magnetic links send /leech
-    
-    To download Youtube links send <a href="https://youtube.com/watch?8088840">Youtube.com</a>
-    and replay /leech
-    """
