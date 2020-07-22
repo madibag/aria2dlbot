6cabb4fe5fead4ae9f3e53dfccfb013406689a6b
madibag <62722024+madibag@users.noreply.github.com>
2020-07-22 23:36:07 +0300

    Update magnet.py

--- tobrot/plugins/magnet.py
+++ tobrot/plugins/magnet.py
@@ -7,8 +7,8 @@ base_url = 'https://sjprojectsapi.herokuapp.com/torrent/?query='
 async def magnet_link_s(client, message):
     search = message.text
     print(search)
+    search = search.split(' ')
     if len(search) > 1:
-        search = search.split(' ')
         search.remove('/Magnet')
         await message.reply_text("Searching Magnet...", quote=True)
         r = requests.get(base_url+str(search)).json()
