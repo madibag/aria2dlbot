541a5cd802d28d1cdd1470bb186a5ac2af71cefa
madibag <62722024+madibag@users.noreply.github.com>
2020-09-11 13:52:33 +0300

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -26,7 +26,6 @@ from tobrot.helper_funcs.extract_link_from_message import extract_link
 from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function, aria_start
 from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
-from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
 from tobrot.helper_funcs.admin_check import AdminCheck
         
 async def incoming_purge_message_f(client, message):
@@ -85,47 +84,4 @@ async def incoming_message_f(client, message):
         )
 
 
-async def incoming_youtube_dl_f(client, message):
-    """ /ytdl command """
-    i_m_sefg = await message.reply_text("processing", quote=True)
-    # LOGGER.info(message)
-    # extract link from message
-    dl_url, cf_name, yt_dl_user_name, yt_dl_pass_word = await extract_link(
-        message.reply_to_message, "YTDL"
-    )
-    LOGGER.info(dl_url)
-    LOGGER.info(cf_name)
-    if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
-        current_user_id = message.from_user.id
-        # create an unique directory
-        user_working_dir = os.path.join(DOWNLOAD_LOCATION, str(current_user_id))
-        # create download directory, if not exist
-        if not os.path.isdir(user_working_dir):
-            os.makedirs(user_working_dir)
-        # list the formats, and display in button markup formats
-        thumb_image, text_message, reply_markup = await extract_youtube_dl_formats(
-            dl_url,
-            # cf_name,
-            yt_dl_user_name,
-            yt_dl_pass_word,
-            user_working_dir
-        )
-        if thumb_image is not None:
-            await message.reply_photo(
-                photo=thumb_image,
-                quote=True,
-                caption=text_message,
-                reply_markup=reply_markup
-            )
-            await i_m_sefg.delete()
-        else:
-            await i_m_sefg.edit_text(
-                text=text_message,
-                reply_markup=reply_markup
-            )
-    else:
-        await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
-            f"<b>API Error</b>: {cf_name}"
-        )
+
